551180a4a614bb0395d867f8b2d891a291c47dca
nhcarrigan <nhcarrigan@gmail.com>
2020-11-11 15:36:19 -0800

    Remove comment blocks
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,15 +1,7 @@
-/*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
-
-var chaiHttp = require('chai-http');
-var chai = require('chai');
-var assert = chai.assert;
-var server = require('../server');
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+const assert = chai.assert;
+const server = require('../server');
 
 chai.use(chaiHttp);
 

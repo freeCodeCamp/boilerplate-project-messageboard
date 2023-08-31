a688fb26a03f14ce5a253d9053b176b77f9c8d60
Alex <aclevinson@gmail.com>
2023-08-31 15:43:38 -0400

    Wrote functional tests

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,13 @@
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+    .post(function(req, res) {
+      res.json(req.body)
+    })
+    .get(function(req, res) {
+      res.json([])
+    })
     
   app.route('/api/replies/:board');
 
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,164 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+    let threadId = ""
+    let replyId = ""
+    suite('TEST /api/threads/{board}', function() {
+        
+        test('POST /api/threads/{board}', function(done) {
+            chai.request(server)
+            .keepOpen()
+            .post('/api/threads/test')
+            .send({
+                text: "Test",
+                password_string: "TestPassword"
+            })
+            .end(function(err, res) {
+                assert.equal(res.status, 200)
+                done()
+            });
+        });
+        test('GET /api/threads/{board}', function(done) {
+            chai.request(server)
+            .keepOpen()
+            .get('/api/threads/test')
+            .end(function(err, res) {
+                assert.equal(res.status, 200)
+                assert.isArray(res.body);
+                assert.isAtMost(res.body.length, 10)
+                assert.property(res.body[0], 'replies')
+                assert.isAtMost(res.body[0].replies.length, 3)
+                threadId = res.body[0]._id
+                done()
+            });
+        });
+        test('DELETE /api/threads/{board} with invalid password', function(done) {
+            chai.request(server)
+            .keepOpen()
+            .delete('/api/threads/test')
+            .send({
+                thread_id: threadId,
+                delete_password: 'invalid password'
+            })
+            .end(function(err, res) {
+                assert.equal(res.status, 200)
+                assert.isString(res.body)
+                assert.equal(res.body, 'incorrect password')
+                done()
+            });
+        });
+        test('DELETE /api/threads/{board} with valid password', function(done) {
+            chai.request(server)
+            .keepOpen()
+            .delete('/api/threads/test')
+            .send({
+                thread_id: threadId,
+                delete_password: 'TestPassword'
+            })
+            .end(function(err, res) {
+                assert.equal(res.status, 200)
+                assert.isString(res.body)
+                assert.equal(res.body, 'success')
+                done()
+            });
+        });
+        test('POST /api/threads/{board}', function(done) {
+            chai.request(server)
+            .keepOpen()
+            .delete('/api/threads/test')
+            .send({
+                thread_id: threadId
+            })
+            .end(function(err, res) {
+                assert.equal(res.status, 200)
+                assert.isString(res.body)
+                assert.equal(res.body, 'reported')
+                done()
+            });
+        });
+    });
+    suite("TEST /api/replies/{board}", function() {
+        test('POST /api/replies/{board}', function(done) {
+            chai.request(server)
+            .keepOpen()
+            .post('/api/replies/test')
+            .send({
+                thread_id: threadId,
+                text: "Test",
+                password_string: "TestPassword"
+            })
+            .end(function(err, res) {
+                assert.equal(res.status, 200)
+                done()
+            });
+        });
+        test('GET /api/replies/{board}?thread_id={thread_id}', function(done) {
+            chai.request(server)
+            .keepOpen()
+            .post('/api/replies/test')
+            .query({
+                "thread_id": threadId
+            })
+            .end(function(err, res) {
+                let replies = []
+                assert.equal(res.status, 200)
+                assert.property(res.body, 'replies')
+                assert.isArray(res.body.replies)
+                replies = res.body.replies
+                replies.forEach(element => {
+                    assert.notProperty(element, 'reported')
+                    assert.notProperty(element, 'delete_password')
+                });
+                replyId = replies[0].id
+                done()
+            });
+        });
+        test('DELETE /api/replies/{board} invalid delete password', function(done) {
+            chai.request(server)
+            .keepOpen()
+            .delete('/api/replies/test')
+            .send({
+                thread_id: threadId,
+                reply_id: replyId,
+                password_string: "Invalid"
+            })
+            .end(function(err, res) {
+                assert.equal(res.status, 200)
+                assert.isString(res.body)
+                assert.equal(res.body, 'incorrect password')
+                done()
+            });
+        });
+        test('DELETE /api/replies/{board} valid delete password', function(done) {
+            chai.request(server)
+            .keepOpen()
+            .delete('/api/replies/test')
+            .send({
+                thread_id: threadId,
+                reply_id: replyId,
+                password_string: "TestPassword"
+            })
+            .end(function(err, res) {
+                assert.equal(res.status, 200)
+                assert.isString(res.body)
+                assert.equal(res.body, 'success')
+                done()
+            });
+        });
+        test('PUT /api/replies/{board}', function(done) {
+            chai.request(server)
+            .keepOpen()
+            .delete('/api/replies/test')
+            .send({
+                thread_id: threadId,
+                reply_id: replyId
+            })
+            .end(function(err, res) {
+                assert.equal(res.status, 200)
+                assert.isString(res.body)
+                assert.equal(res.body, 'reported')
+                done()
+            });
+        });
+    });
 });
